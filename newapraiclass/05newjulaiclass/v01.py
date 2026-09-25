776de113369c0250aeda5d785993ca0cf0785bc8
Ajit Mandal <iamajitmandal.me@gmail.com>
2026-09-25 19:14:08 +0545

    revised v01 and v02 in newapraiclass

---
+++ newapraiclass/05newjulaiclass/v01.py
@@ -0,0 +1,60 @@
+# 16th July, 2026
+# Learned Introduction to AI & ML
+
+# Introduction to AI & ML
+
+# Intelligence
+
+# Tesla Robot
+
+# How is human intelligence different from other animals?
+
+# AI: Ability of man-made agents to do tasks traditionally attributed to "intelligence" of some level.
+#     If we build intelligent machines, how can we know if it is really intelligent?
+
+# Alan Turing(1950):
+# Turing Test: The Turing Test is a method to check if a machine can show human-like intelligence
+# through conversation. English mathematician Alan Turing proposed the test in 1950 in his paper
+# "Computing Machinery and Intelligence". He originally called it the "Imitation Game".
+
+# How the Test Works?
+# Three Players: The test uses a human judge (the interrogator), another human, and a machine.
+# Text Chat: All three players stay in separate rooms and talk only through text-based messages.
+# The Goal: The judge asks questions to both participants to figure out which one is the human and
+# which one is the machine.
+# Passing the Test: If the judge cannot tell the machine and the human apart based on their answers,
+# the machine passes.
+
+# Different Levels of Artificial Intelligence: (Old course/old models so we can skip it)
+# Reflex-based models
+# State-based models
+# Variables-based models
+# Logic-based models
+
+# check the website: https://thispersondoesnotexist.com/
+
+# Goal of any AI System: to extract some information from availabe data & solve problems
+
+# Applications of AI:
+# Computer Vision
+# Computer Audition
+# NLP
+
+# Machine Learning:
+    # According to computer scientist Tom M. Mitchell, machine learning is defined formally
+    # as:"A computer program is said to learn from experience E with respect to some class of
+    # tasks T and performance measure P if its performance at tasks in T, as measured by P,
+    # improves with experience E."
+# Why ML:
+
+# AI hallucination: An AI hallucination is an AI hallucination response where an artificial intelligence
+# system confidently generates false, misleading, or fabricated information presented as absolute fact.
+
+# Types: Supervised, Unsupervised, Reinforcement Learning
+# Benefits of ML
+
+# Issues of ML
+
+# "DATA IS THE NEW GOLD"
+
+# Differences between AI, ML, DL & Data Science
---
+++ newapraiclass/05newjulaiclass/v03.py
@@ -0,0 +1,6 @@
+# 19th July, 2026
+# Learned
+
+# Loss in Linear Regression Model
+# Types of Loss in Linear Regression Model
+# Choosing the loss in Linear regression model
